40797d35913e1325048fdb225bb49a09ce638065
IRact <valhdz-2022@outlook.com>
2026-06-09 22:03:12 -0600

    reorganizacion y pruebas + pagina login y registro creadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,30 @@
 import { Routes } from '@angular/router';
-import { Layout } from './components/layout/layout';
 
 export const routes: Routes = [
-    {},
     {
-        path: 'layout',
-        component: Layout,
-    }
+        path: '',
+        loadComponent: () => import('./shared/components/layout/layout').then(m => m.Layout),
+        children: [
+            {
+                path: '',
+                redirectTo: '/dashboard',
+                pathMatch: 'full',
+            },
+        ]
+    },
+
+    {
+        path: 'login',
+        loadComponent: () => import('./features/auth/login/login').then(m => m.Login),
+    },
+
+    {
+        path: 'register',
+        loadComponent: () => import('./features/auth/register/register').then(m => m.Register),
+    },
+
+    {
+        path: '**',
+        redirectTo: 'dashboard',
+    },
 ];
